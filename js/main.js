3b47963070cc5d84d45b9c9ec93cc922dfd1ac90
Sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2020-11-15 23:16:16 +0100

    js comments

--- js/main.js
+++ js/main.js
@@ -9,27 +9,32 @@ document.addEventListener("DOMContentLoaded", function () {
     let btn2 = document.getElementById("closebtn")
     let link = document.querySelector('.overlay-content')
 
+    
+    //function to show and hide my projects
     minaBtn.addEventListener("click",function (){
         p2.classList.add("hidden");
         p.classList.remove("hidden");
         
     });
 
+    //function to show and hide school projects
     skolBtn.addEventListener("click",function (){
         p.classList.add("hidden");
         p2.classList.remove("hidden");
         
     });
 
-
+    //function to show nav menu
     btn.addEventListener("click", function(){
         document.getElementById("myNav").style.width = "100%";
     })
-        
+      
+    //function to hide nav menu
     btn2.addEventListener("click", function(){
         document.getElementById("myNav").style.width = "0%";
     })
 
+    //function to hide nav menu after clicking the nav links
     link.addEventListener("click", function(){
         document.getElementById("myNav").style.width = "0%";
     })

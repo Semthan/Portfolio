22c93aff527e03b5c2bb361f8fea8b1c6c8c36c7
sarmad Al-Bidhawi <sarmed.91@hotmail.com>
2021-07-06 18:55:56 +0200

    channged variables names and added more cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
-    
-    let minaBtn = document.getElementById("mina");
-    let skolBtn = document.getElementById("skol");
+
+    let selectedBtn = document.getElementById("selected");
+    let upcomingBtn = document.getElementById("upcoming");
     let p = document.getElementById("projects");
     let p2 = document.getElementById("projects2");
 
@@ -9,34 +9,34 @@ document.addEventListener("DOMContentLoaded", function () {
     let btn2 = document.getElementById("closebtn")
     let link = document.querySelector('.overlay-content')
 
-    
+
     //function to show and hide my projects
-    minaBtn.addEventListener("click",function (){
+    selectedBtn.addEventListener("click", function () {
         p2.classList.add("hidden");
         p.classList.remove("hidden");
-        
+
     });
 
     //function to show and hide school projects
-    skolBtn.addEventListener("click",function (){
+    upcomingBtn.addEventListener("click", function () {
         p.classList.add("hidden");
         p2.classList.remove("hidden");
-        
+
     });
 
     //function to show nav menu
-    btn.addEventListener("click", function(){
+    btn.addEventListener("click", function () {
         document.getElementById("myNav").style.width = "100%";
     })
-      
+
     //function to hide nav menu
-    btn2.addEventListener("click", function(){
+    btn2.addEventListener("click", function () {
         document.getElementById("myNav").style.width = "0%";
     })
 
     //function to hide nav menu after clicking the nav links
-    link.addEventListener("click", function(){
+    link.addEventListener("click", function () {
         document.getElementById("myNav").style.width = "0%";
     })
 
-  });
+});
